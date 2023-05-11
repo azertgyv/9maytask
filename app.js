74bcbe7a47110214f077a483dd5be1f5c0572d8a
azertgyv <atagiyev077@gmail.com>
2023-05-11 10:28:29 +0400

    9maytask

---
+++ app.js
@@ -0,0 +1,59 @@
+// TASK 1
+
+// const arr = []
+// for (let i = 0; i < 5; i++) {
+//     let num = +prompt(`Arrayi daxil edin${i + 1}: `)
+//     arr.push(Number(num))
+// }
+// console.log(arr)
+
+// -----------------------------------------------------
+// TASK 2
+
+
+// const arr = [2, 8, 2]
+// let result = 1
+// for (let i = 0; i < arr.length; i++) {
+//     result *= arr[i]
+// }
+// console.log(result)
+
+// ----------------------------------------------------------
+
+// TASK 3
+
+// const multiply = (...arg) => {
+//     let result = 1
+//     for (let i = 0; i < arg.length; i++) {
+//         result *= arg[i]
+//     }
+//     console.log(result)
+// }
+
+// multiply(2, 4, 1,)
+
+// ---------------------------------------------------
+
+// TASK 4
+
+
+const convertElementsToItsLengths = ('adasf','asfasfa','asfasfasfagadg'){
+let arr = [];
+  for (let i = 0; i < convertElementsToItsLengths.length; i++) {
+    arr.push(arr[i].length);
+
+}
+    return arr;
+}
+
+
+// console.log(arr) // convertElementsToItsLengths(arr)
+
+// function convertElementsToItsLengths(arr) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       result.push(arr[i].length);
+//     }
+//     return result;
+//   }
+  
